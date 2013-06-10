bd1f5232bc518be259de20d066d837f5ac72c6bf
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2013-06-10 08:26:58 +0200

    Commit for example on wrong error line number.

--- linenoise.go
+++ linenoise.go
@@ -7,10 +7,33 @@ package linenoise
 import "C"
 
 import (
-	"fmt"
+	"unsafe"
 )
 
-// DoSomething does something
-func DoSomething() {
-	fmt.Println("hi")
+// Line displays given string and returns line from user input.
+// char *linenoise(const char *prompt);
+func Line(prompt string) string {
+	promptCString := C.CString(prompt)
+	resultCString := C.linenoise(promptCString) // will error on this line
+	C.free(promptCString)                       // but should error on this line
+
+	result := C.GoString(resultCString)
+	C.free(unsafe.Pointer(resultCString)) // TODO: is this required?
+	return result
 }
+
+// int linenoiseHistoryAdd(const char *line);
+// int linenoiseHistorySetMaxLen(int len);
+// int linenoiseHistorySave(char *filename);
+// int linenoiseHistoryLoad(char *filename);
+// void linenoiseClearScreen(void);
+// void linenoiseSetMultiLine(int ml);
+
+// typedef struct linenoiseCompletions {
+//   size_t len;
+//   char **cvec;
+// } linenoiseCompletions;
+
+// typedef void(linenoiseCompletionCallback)(const char *, linenoiseCompletions *);
+// void linenoiseSetCompletionCallback(linenoiseCompletionCallback *);
+// void linenoiseAddCompletion(linenoiseCompletions *, char *);
