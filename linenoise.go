f8ce3282fd53374e11abbf4f40604769a6d4825e
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2013-06-10 09:21:49 +0200

    Added wrapping functions.

--- linenoise.go
+++ linenoise.go
@@ -7,6 +7,7 @@ package linenoise
 import "C"
 
 import (
+	"errors"
 	"unsafe"
 )
 
@@ -14,20 +15,75 @@ import (
 // char *linenoise(const char *prompt);
 func Line(prompt string) string {
 	promptCString := C.CString(prompt)
-	resultCString := C.linenoise(promptCString) // will error on this line
-	C.free(promptCString)                       // but should error on this line
+	resultCString := C.linenoise(promptCString)
+	C.free(unsafe.Pointer(promptCString))
 
 	result := C.GoString(resultCString)
 	C.free(unsafe.Pointer(resultCString)) // TODO: is this required?
 	return result
 }
 
+// AddHistory adds a line to history
 // int linenoiseHistoryAdd(const char *line);
+func AddHistory(line string) error {
+	lineCString := C.CString(line)
+	res := C.linenoiseHistoryAdd(lineCString)
+	C.free(unsafe.Pointer(lineCString))
+	if res != 1 {
+		return errors.New("Could not add line to history.")
+	}
+	return nil
+}
+
+// SetHistoryMaxLen changes the maximum length of history
 // int linenoiseHistorySetMaxLen(int len);
+func SetHistoryMaxLen(capacity int) error { // TODO: maybe rename to SetHistoryCapacity
+	res := C.linenoiseHistorySetMaxLen(C.int(capacity))
+	if res != 1 {
+		return errors.New("Could not set history max len.")
+	}
+	return nil
+}
+
+// SaveHistory saves from file with given filename.
 // int linenoiseHistorySave(char *filename);
+func SaveHistory(filename string) error {
+	filenameCString := C.CString(filename)
+	res := C.linenoiseHistorySave(filenameCString)
+	C.free(unsafe.Pointer(filenameCString))
+	if res != 1 {
+		return errors.New("Could not save history to file.")
+	}
+	return nil
+}
+
+// LoadHistory loads from file with given filename.
 // int linenoiseHistoryLoad(char *filename);
+func LoadHistory(filename string) error {
+	filenameCString := C.CString(filename)
+	res := C.linenoiseHistoryLoad(filenameCString)
+	C.free(unsafe.Pointer(filenameCString))
+	if res != 1 {
+		return errors.New("Could not load history from file.")
+	}
+	return nil
+}
+
+// Clear clears the screen.
 // void linenoiseClearScreen(void);
+func Clear() {
+	C.linenoiseClearScreen()
+}
+
+// SetMultiline sets linenoise to multiline or single line
 // void linenoiseSetMultiLine(int ml);
+func SetMultiline(ml bool) {
+	if ml {
+		C.linenoiseSetMultiLine(1)
+	} else {
+		C.linenoiseSetMultiLine(0)
+	}
+}
 
 // typedef struct linenoiseCompletions {
 //   size_t len;
